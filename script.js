ec9ba68c80d71b1aa1a4a54408b258b15feba678
mathieu belson <mathieu.belson@gmail.com>
2023-09-21 14:53:12 +0900

    weather icons if statement added

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const apiKey = "a320d8486cc2177266f45bfcaa6738d6"; // Your API key here
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=tokyo";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`); // Fetch data from API. await is a keyword that waits for the promise to be resolved.
+const searchField = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+
+async function checkWeather(city) {  // the city will come from the input field
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`); // Fetch data from API. await is a keyword that waits for the promise to be resolved.
     var data = await response.json(); // Convert data to JSON.
     
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } 
+    else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchField.value);
+});
